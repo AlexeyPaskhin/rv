b6fe65fc4f91992335eebdb0f93f294c71b324a8
yr <yr@playtini.ua>
2018-01-16 12:04:51 +0200

    added simple social registration for Yandex and Odnoklasniki

--- src/main/java/com/popups/FastRegisterPopup.java
+++ src/main/java/com/popups/FastRegisterPopup.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.By;
 
 import static com.utils.DriverManager.getDriver;
 
-public class FastRegisterPopup extends AbstractPage  {
+public class FastRegisterPopup extends AbstractPage {
     private static final InputBox ENTER_EMAIL_INPUT = new InputBox(By.id("register-form-login"));
     private static final InputBox ENTER_PASS_INPUT = new InputBox(By.id("register-form-password"));
     private static final Button REGISTER_BUTTON = new Button(By.xpath("//button[@class='btn-popup-register']/span"));
@@ -17,60 +17,69 @@ public class FastRegisterPopup extends AbstractPage  {
     private static final Checkbox CURRENCY_USD_CHECKBOX = new Checkbox(By.xpath("//div[@id='popup_register']//input[@name='currency' and @value='USD']"));
     private static final Checkbox AGREE_CHECKBOX = new Checkbox(By.xpath("//div[@id='popup_register']//input[@name='agree' and @type='checkbox']"));
 
-    private final Button VK_BUTTON = new Button(By.xpath("//div[@id='popup_register']//div[@class='social-vk']"));
-    private final Button MAILRU_BUTTON = new Button(By.xpath("//div[@id='popup_register']//div[@class='social-mr']"));
-
     private static final Button VK_BUTTON_HOME_PAGE = new Button(By.xpath("//div[@id='popup_register']//div[@class='social-vk']"));
     private static final Button FB_BUTTON_HOME_PAGE = new Button(By.xpath("//div[@id='popup_register']//div[@class='social-fb']"));
     private static final Button OK_BUTTON_HOME_PAGE = new Button(By.xpath("//div[@id='popup_register']//div[@class='social-ok']"));
     private static final Button YA_BUTTON_HOME_PAGE = new Button(By.xpath("//div[@id='popup_register']//div[@class='social-ya']"));
     private static final Button MAILRU_BUTTON_HOME_PAGE = new Button(By.xpath("//div[@id='popup_register']//div[@class='social-mr']"));
 
-    public FastRegisterPopup typeLogin(String login){
+    public FastRegisterPopup typeLogin(String login) {
         ENTER_EMAIL_INPUT.fillIn(login);
         return this;
     }
 
-    public FastRegisterPopup typePass(String pass){
+    public FastRegisterPopup typePass(String pass) {
         ENTER_PASS_INPUT.fillIn(pass);
         return this;
     }
 
-    public FastRegisterPopup selectCurrencyRUB(){
+    public FastRegisterPopup selectCurrencyRUB() {
         CURRENCY_RUB_CHECKBOX.click();
         return this;
-
     }
 
-    public FastRegisterPopup selectCurrencyUSD(){
+    public FastRegisterPopup selectCurrencyUSD() {
         CURRENCY_USD_CHECKBOX.click();
         return this;
-
     }
-    public FastRegisterPopup agreeWithRules(){
+
+    public FastRegisterPopup agreeWithRules() {
         AGREE_CHECKBOX.click();
         return this;
     }
 
-    public GiftPopup clickRegisterButton(){
+    public GiftPopup clickRegisterButton() {
         REGISTER_BUTTON.clickUntilDisappeared();
         return new GiftPopup();
     }
 
     public SocialFrame clickVK() {
-        VK_BUTTON.click();
+        VK_BUTTON_HOME_PAGE.click();
         swithToSocialFrame();
         return new VkRegisterPage();
     }
 
     public SocialFrame clickMailRu() {
-        MAILRU_BUTTON.click();
+        MAILRU_BUTTON_HOME_PAGE.click();
         swithToSocialFrame();
         return new MailRuRegisterPage();
     }
+
     public SocialFrame clickFB() {
         FB_BUTTON_HOME_PAGE.click();
         swithToSocialFrame();
         return new FBregisterPage();
+    }
+
+    public SocialFrame clickOK() {
+        OK_BUTTON_HOME_PAGE.click();
+        swithToSocialFrame();
+        return new OKRegisterPage();
+    }
+
+    public SocialFrame clickYA() {
+        YA_BUTTON_HOME_PAGE.click();
+        swithToSocialFrame();
+        return new YARegisterPage();
     }
 }
